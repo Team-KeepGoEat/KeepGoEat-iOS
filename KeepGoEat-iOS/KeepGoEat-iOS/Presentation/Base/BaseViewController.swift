b0a53575de08343dd61a75567de39ad372864901
jeongkite <jeong6kite@gmail.com>
2022-12-30 11:06:23 +0900

    Update [#1] BaseVC 배경색 및 네비게이션바 설정

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Base/BaseViewController.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Base/BaseViewController.swift
@@ -12,18 +12,18 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        style()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        setNavigationBar()
     }
-    */
-
+    
+    func style() {
+        view.backgroundColor = .white
+    }
+    
+    func setNavigationBar() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 }
