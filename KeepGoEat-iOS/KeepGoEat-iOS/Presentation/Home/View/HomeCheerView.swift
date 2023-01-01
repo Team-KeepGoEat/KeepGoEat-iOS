1c4d284d9ea9e1d9a5127ca4ccc041ba4b23cf1d
jeongkite <jeong6kite@gmail.com>
2023-01-01 18:55:14 +0900

    Add [#3] Home Cheer View

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeCheerView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeCheerView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/01.
+//
+
+import UIKit
+
+class HomeCheerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
