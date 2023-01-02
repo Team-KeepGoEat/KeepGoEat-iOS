1d03a977641a2ff4a7aae4cb5008ada887128de2
jeongkite <jeong6kite@gmail.com>
2023-01-02 21:06:15 +0900

    Chore [#8] 컨벤션에 맞게 탭 간격 4로 수정

--- KeepGoEat-iOS/KeepGoEat-iOS/Common/Consts/Font.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Common/Consts/Font.swift
@@ -8,57 +8,57 @@
 import UIKit.UIFont
 
 extension UIFont {
-
-  class var systemDate: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 48.0)!
-  }
-
-  class var system1Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 24.0)!
-  }
-
-  class var system1: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 24.0)!
-  }
-
-  class var system2Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 20.0)!
-  }
-
-  class var system2: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 20.0)!
-  }
-
-  class var system3Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 18.0)!
-  }
-
-  class var system3: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 18.0)!
-  }
-
-  class var system4Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 16.0)!
-  }
-
-  class var system4: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 16.0)!
-  }
-
-  class var system5Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 14.0)!
-  }
-
-  class var system5: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 14.0)!
-  }
-
-  class var system6Bold: UIFont {
-    return UIFont(name: "Pretendard-Bold", size: 12.0)!
-  }
-
-  class var system6: UIFont {
-    return UIFont(name: "Pretendard-Regular", size: 12.0)!
-  }
-
+    
+    class var systemDate: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 48.0)!
+    }
+    
+    class var system1Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 24.0)!
+    }
+    
+    class var system1: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 24.0)!
+    }
+    
+    class var system2Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 20.0)!
+    }
+    
+    class var system2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 20.0)!
+    }
+    
+    class var system3Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 18.0)!
+    }
+    
+    class var system3: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 18.0)!
+    }
+    
+    class var system4Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 16.0)!
+    }
+    
+    class var system4: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16.0)!
+    }
+    
+    class var system5Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 14.0)!
+    }
+    
+    class var system5: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14.0)!
+    }
+    
+    class var system6Bold: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 12.0)!
+    }
+    
+    class var system6: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 12.0)!
+    }
+    
 }
