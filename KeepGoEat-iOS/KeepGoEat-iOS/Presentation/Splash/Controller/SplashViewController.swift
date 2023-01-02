f45217b169364b01b46d09109b2415d21b4c3f63
lsh328328 <lsh328328@naver.com>
2023-01-02 13:04:51 +0900

    Setting [#7] 스플래쉬 뷰 폴더링 및 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Splash/Controller/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  KeepGoEat-iOS
+//
+//  Created by 이승헌 on 2023/01/01.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Splash/View/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 이승헌 on 2023/01/02.
+//
+
+import UIKit
