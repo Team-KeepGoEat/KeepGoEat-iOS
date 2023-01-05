b26f17ab8226907f4f96ee9a5e755ab848085710
jeongkite <jeong6kite@gmail.com>
2023-01-06 04:55:33 +0900

    Style [#18] EmptyView UI 구성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeEmptyView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeEmptyView.swift
@@ -16,7 +16,7 @@ class HomeEmptyView: UIView {
     // MARK: Component
     private let homeBaseView = HomeBaseView()
     private let emptyImage = UIImageView().then {
-        $0.setImage(Const.Image.plateGrayView1)
+        $0.image = Const.Image.plateGrayView1
     }
     private let emptyTitleLabel = UILabel().then {
         $0.text = Const.String.homeEmptyTitle
@@ -27,6 +27,14 @@ class HomeEmptyView: UIView {
         $0.text = Const.String.homeEmptySub
         $0.font = .system4
         $0.textColor = .gray600
+    }
+    private let addGoalButton = UIButton().then {
+        $0.backgroundColor = .orange600
+        $0.setTitle(Const.String.addGoal, for: .normal)
+        $0.setImage(Const.Image.icnPlus, for: .normal)
+        $0.setTitleColor(.gray50, for: .normal)
+        $0.titleLabel?.font = .system4Bold
+        $0.layer.cornerRadius = 8
     }
     
     // MARK: - Function
@@ -47,10 +55,33 @@ class HomeEmptyView: UIView {
         self.addSubviews(
             homeBaseView
         )
+        homeBaseView.addSubviews(
+            emptyImage,
+            emptyTitleLabel,
+            emptySubLabel,
+            addGoalButton
+        )
         
         homeBaseView.snp.makeConstraints {
-            $0.top.equalTo(homeCheerView.snp.bottom).inset(5.adjusted)
-            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.edges.equalToSuperview()
+        }
+        emptyImage.snp.makeConstraints {
+            $0.width.height.equalTo(140.adjusted)
+            $0.top.equalTo(homeBaseView.homeHeaderLabel.snp.bottom).inset(-30.adjusted)
+            $0.centerX.equalToSuperview()
+        }
+        emptyTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(emptyImage.snp.bottom).inset(-8.adjusted)
+            $0.centerX.equalToSuperview()
+        }
+        emptySubLabel.snp.makeConstraints {
+            $0.top.equalTo(emptyTitleLabel.snp.bottom).inset(-12.adjusted)
+            $0.centerX.equalToSuperview()
+        }
+        addGoalButton.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(16.adjusted)
+            $0.bottom.equalToSuperview().inset(44.adjusted)
+            $0.height.equalTo(48.adjusted)
         }
     }
 }
