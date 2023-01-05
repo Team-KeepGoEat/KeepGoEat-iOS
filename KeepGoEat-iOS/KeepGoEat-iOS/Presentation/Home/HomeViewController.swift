13b74b181d10e5b62128debb55653d00c83d35f7
jeongkite <jeong6kite@gmail.com>
2023-01-05 21:26:08 +0900

    Style [#3] 홈 응원 영역 구성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/HomeViewController.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/HomeViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - Variables
+    // MARK: Component
+    private let homeCheerView = HomeCheerView()
 
+    // MARK: - Function
+    // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
+        setLayout()
     }
     
+    // MARK: Layout Helpers
+    private func setUI() {
+        view.backgroundColor = .gray700
+    }
+    private func setLayout() {
+        view.addSubviews(
+            homeCheerView
+        )
+        homeCheerView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: Preview
+struct ViewControllerPreView: PreviewProvider {
+    static var previews: some View {
+        HomeViewController().toPreview()
+    }
 }
