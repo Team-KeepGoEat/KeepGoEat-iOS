b250da5263c659aba535952ff647c5c137a76564
jeongkite <jeong6kite@gmail.com>
2023-01-06 03:19:54 +0900

    Style [#18] 홈 Base View UI 구성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeBaseView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeBaseView.swift
@@ -5,4 +5,53 @@
 //  Created by 양정연 on 2023/01/06.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class HomeBaseView: UIView {
+    
+    // MARK: - Variables
+    // MARK: Component
+    private let homeHeaderLabel = UILabel().then {
+        $0.text = Const.String.hoemHeader
+        $0.font = .system2Bold
+        $0.textColor = .gray700
+    }
+    
+    // MARK: - Function
+    // MARK: LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Layout Helpers
+    private func setUI() {
+        self.backgroundColor = .white
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 4.adjusted
+        self.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+    }
+    
+    private func setLayout() {
+        self.addSubviews(
+            homeHeaderLabel
+        )
+        
+        self.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(240.adjusted)
+            $0.horizontalEdges.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+        homeHeaderLabel.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top).inset(20.adjusted)
+            $0.leading.equalTo(self.snp.leading).inset(16.adjusted)
+        }
+    }
+}
