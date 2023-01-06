9c7bea21f2eb3dc8ef3cee55b75c79eee641c706
jeongkite <jeong6kite@gmail.com>
2023-01-07 05:21:46 +0900

    Chore [#16] API 변경 데이터 모델에 추가

--- KeepGoEat-iOS/KeepGoEat-iOS/Data/Home/HomeResponse.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/Home/HomeResponse.swift
@@ -12,6 +12,7 @@ struct GetHomeResponse: Codable {
     let goals: [Goal]
     let goalCount: Int
     let cheeringMessage: String
+    let dayTime: Int
 }
 
 // MARK: Goal
@@ -27,24 +28,24 @@ struct Goal: Codable {
 
 extension GetHomeResponse {
     static func setHomeDummy0() -> GetHomeResponse {
-        return GetHomeResponse(goals: [], goalCount: 0, cheeringMessage: "오늘도 0개 행복하게 내일 더 멋지게!")
+        return GetHomeResponse(goals: [], goalCount: 0, cheeringMessage: "오늘도 0개 행복하게 내일 더 멋지게!", dayTime: 0)
     }
     static func setHomeDummy1() -> GetHomeResponse {
         return GetHomeResponse(goals: [
             Goal(goalID: 1, goalContent: "하루 한 번 유산균", isMore: true, isOngoing: true, writerID: 1, totalCount: 7, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 6)
-        ], goalCount: 1, cheeringMessage: "오늘도 1개 행복하게 내일 더 멋지게!")
+        ], goalCount: 1, cheeringMessage: "오늘도 1개 행복하게 내일 더 멋지게!", dayTime: 1)
     }
     static func setHomeDummy2() -> GetHomeResponse {
         return GetHomeResponse(goals: [
             Goal(goalID: 1, goalContent: "하루 한 번 유산균", isMore: true, isOngoing: true, writerID: 1, totalCount: 7, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 6),
-            Goal(goalID: 3, goalContent: "군것질거리", isMore: false, isOngoing: true, writerID: 1, totalCount: 4, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 3)
-        ], goalCount: 2, cheeringMessage: "오늘도 2개 행복하게 내일 더 멋지게!")
+            Goal(goalID: 3, goalContent: "군것질거리", isMore: false, isOngoing: true, writerID: 1, totalCount: 4, startedAt: "2022. 08.10", keptAt: "", isAchieved: true, thisMonthCount: 3)
+        ], goalCount: 2, cheeringMessage: "오늘도 2개 행복하게 내일 더 멋지게!", dayTime: 2)
     }
     static func setHomeDummy3() -> GetHomeResponse {
         return GetHomeResponse(goals: [
             Goal(goalID: 1, goalContent: "하루 한 번 유산균", isMore: true, isOngoing: true, writerID: 1, totalCount: 7, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 6),
-            Goal(goalID: 2, goalContent: "라면", isMore: false, isOngoing: true, writerID: 1, totalCount: 20, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 17),
+            Goal(goalID: 2, goalContent: "라면", isMore: false, isOngoing: true, writerID: 1, totalCount: 20, startedAt: "2022. 08.10", keptAt: "", isAchieved: true, thisMonthCount: 17),
             Goal(goalID: 3, goalContent: "군것질거리", isMore: false, isOngoing: true, writerID: 1, totalCount: 4, startedAt: "2022. 08.10", keptAt: "", isAchieved: false, thisMonthCount: 3)
-        ], goalCount: 3, cheeringMessage: "오늘도 3개 행복하게 내일 더 멋지게!")
+        ], goalCount: 3, cheeringMessage: "오늘도 3개 행복하게 내일 더 멋지게!", dayTime: 1)
     }
 }
