857dd0195d59e89f3a21091a13b6ea8dc90613c2
Gayeonchoi <105284376+Gayeonchoi@users.noreply.github.com>
2023-01-06 16:01:43 +0900

    Feat [#13] onboarding - 온보딩 뷰 기본 셋팅

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/Onboarding/OnboardingDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingDataModel.swift
+//  KeepGoEat-iOS
+//
+//  Created by 최가연 on 2023/01/06.
+//
+
+struct OnboardingDataModel {
+    let title: String
+    let descrtiption: String
+    let animation: String
+}
---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Onboarding/Cell/OnboardingCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  KeepGoEat-iOS
+//
+//  Created by 최가연 on 2023/01/06.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
