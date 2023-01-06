31d69e962a55317a944c45f6d6dd77580cff3d62
jeongkite <jeong6kite@gmail.com>
2023-01-07 02:35:29 +0900

    Add [#16] 홈 목표 카드 컬렉션뷰 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeGoalCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeGoalCollectionView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/07.
+//
+
+import UIKit
+
+class HomeGoalCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
