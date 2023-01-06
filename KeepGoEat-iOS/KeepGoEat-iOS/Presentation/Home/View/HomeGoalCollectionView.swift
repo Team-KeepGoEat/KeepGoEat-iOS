966f9f3aaba739e63058bdcbe8d3b9dc8720319e
jeongkite <jeong6kite@gmail.com>
2023-01-07 06:21:04 +0900

    Feat [#16] 컬렉션뷰 구성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeGoalCollectionView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeGoalCollectionView.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class HomeGoalCollectionView: UICollectionView {
+    
+    // MARK: - Variables
+    let data = GetHomeResponse.setHomeDummy3()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        setRegister()
     }
-    */
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setRegister() {
+        self.dataSource = self
+        self.register(HomeGoalCollectionViewCell.self, forCellWithReuseIdentifier: HomeGoalCollectionViewCell.identifier)
+    }
+}
 
+// MARK: - extension
+// MARK: UICollectionViewDataSource
+extension HomeGoalCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let goalCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeGoalCollectionViewCell.identifier, for: indexPath) as? HomeGoalCollectionViewCell else { return UICollectionViewCell() }
+        goalCell.databind(data: data.goals[indexPath.item])
+        return goalCell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.goalCount
+    }
 }
