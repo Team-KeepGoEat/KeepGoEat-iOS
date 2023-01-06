295402c81731c2d1b0676ff49686e15a19cba420
jeongkite <jeong6kite@gmail.com>
2023-01-06 23:06:32 +0900

    Add [#16] 홈 목표 collectionview cell 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/Cell/HomeGoalCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeGoalCollectionViewCell.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/06.
+//
+
+import UIKit
+
+class HomeGoalCollectionViewCell: UICollectionViewCell {
+    
+}
