6f2009f2fb6ca04c940056cd6be0f729bbfa70c2
lsh328328 <lsh328328@naver.com>
2023-01-06 20:58:53 +0900

    Feat [#30] 스플래쉬 뷰 & 로그인 뷰 기기대응

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Login/Controller/LoginViewController.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Login/Controller/LoginViewController.swift
@@ -17,19 +17,11 @@ class LoginViewController: UIViewController {
     }
     
     private let loginButton: SocialLoginButton = SocialLoginButton(frame: CGRect(), socialType: .kakao)
-    
-    private let logoutButton: UIButton = UIButton().then {
-        $0.backgroundColor = .kakako
-        $0.setTitle("로그아웃", for: .normal)
-        $0.titleLabel?.textAlignment = .center
-        $0.layer.cornerRadius = 8
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
-        setAddTarget()
     }
 }
 
@@ -41,7 +33,6 @@ extension LoginViewController {
     private func setLayout() {
         view.addSubviews(
             loginImageView,
-            logoutButton,
             loginButton
         )
         
@@ -51,35 +42,10 @@ extension LoginViewController {
             $0.centerY.equalTo(view.safeAreaLayoutGuide)
             $0.centerX.equalTo(view.safeAreaLayoutGuide)
         }
-        
-        logoutButton.snp.makeConstraints {
-            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
-            $0.top.equalTo(loginImageView.snp.bottom).inset(-34)
-            $0.height.equalTo(48)
-        }
-        
+
         loginButton.snp.makeConstraints {
-            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
-            $0.bottom.equalToSuperview().inset(34)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(16.adjusted)
+            $0.bottom.equalToSuperview().inset(44.adjusted)
         }
-    }
-    
-    private func setAddTarget() {
-        logoutButton.addTarget(self, action: #selector(tapLogoutButton), for: .touchUpInside)
-    }
-    
-    private func kakaoLogout() {
-        UserApi.shared.logout {(error) in
-            if let error = error {
-                print(error)
-            } else {
-                print("logout() success.")
-            }
-        }
-    }
-    
-    @objc
-    private func tapLogoutButton() {
-        kakaoLogout()
     }
 }
