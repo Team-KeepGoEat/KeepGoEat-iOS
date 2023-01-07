ce98e624154cb47a73c4f6f52844b9cfb0f82fab
jeongkite <jeong6kite@gmail.com>
2023-01-08 05:35:06 +0900

    Add [#32] 필터 버튼 뷰 추가

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreFilterButton.swift
@@ -0,0 +1,20 @@
+//
+//  StoreFilterButton.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/08.
+//
+
+import UIKit
+
+class StoreFilterButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
