8a19eb7d5e846149e9c1289e6ac276c499e60808
jeongkite <jeong6kite@gmail.com>
2023-01-08 05:05:53 +0900

    Style [#32] 컴포넌트 생성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreGoalView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreGoalView.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class StoreGoalView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: Component
+    private let headerView = HeaderView()
+    private let headerLabel = UILabel().then {
+        $0.font = .system4Bold
+        $0.textColor = .gray800
+        $0.text = Const.String.storeTitle
+    }
+    private let totalButton = UIButton().then {
+        $0.titleLabel?.font = .system5
+        $0.setTitle("전체", for: .normal)
+        $0.setTitleColor(.gray800, for: .normal)
+        $0.contentEdgeInsets = UIEdgeInsets(top: 2, left: 5, bottom: 2, right: 5
+    }
+    private let moreButton = UIButton().then {
+        $0.titleLabel?.font = .system5
+        $0.setTitle("더먹기", for: .normal)
+        $0.setTitleColor(.gray400, for: .normal)
+    }
+    private let lessButton = UIButton().then {
+        $0.titleLabel?.font = .system5
+        $0.setTitle("덜먹기", for: .normal)
+        $0.setTitleColor(.gray400, for: .normal)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.backgroundColor = .gray50
     }
     */
 
