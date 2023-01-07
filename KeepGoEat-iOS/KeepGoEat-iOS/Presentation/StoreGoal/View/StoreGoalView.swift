884f05507951904276850fa3a8031adc90c0b871
jeongkite <jeong6kite@gmail.com>
2023-01-08 05:03:47 +0900

    Add [#32] 뷰 및 셀 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreGoalView.swift
@@ -0,0 +1,20 @@
+//
+//  StoreGoalView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/08.
+//
+
+import UIKit
+
+class StoreGoalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
