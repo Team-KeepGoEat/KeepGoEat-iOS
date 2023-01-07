6f98c3802c682ad838a56123fbf115148f8ef1a1
jeongkite <jeong6kite@gmail.com>
2023-01-08 05:36:17 +0900

    Style [#32] 필터 버튼 UI

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreFilterButton.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreFilterButton.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
+enum SelectType: String {
+    case seleted
+    case unselected
+}
+
 class StoreFilterButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let selectType: SelectType
+    
+    init(frame: CGRect, title: String, selectType: SelectType) {
+        self.selectType = selectType
+        super.init(frame: frame)
+        
+        setUI(title: title, selectType: selectType)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    private func setUI(title: String, selectType: SelectType) {
+        var config = UIButton.Configuration.plain()
+        config.background.backgroundColor = .gray50
+        config.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 5, bottom: 2, trailing: 5)
+        switch selectType {
+        case .seleted:
+            config.baseForegroundColor = .gray800
+            config.attributedTitle = AttributedString(title, attributes: AttributeContainer([NSAttributedString.Key.font: UIFont.system5Bold]))
+        case .unselected:
+            config.baseForegroundColor = .gray400
+            config.attributedTitle = AttributedString(title, attributes: AttributeContainer([NSAttributedString.Key.font: UIFont.system5]))
+        }
+        self.configuration = config
+    }
 }
