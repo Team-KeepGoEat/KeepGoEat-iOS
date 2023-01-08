b8d1f5e2747d67430814e2e5e88565d8a1372174
jeongkite <jeong6kite@gmail.com>
2023-01-08 16:08:04 +0900

    Add [#37] 보관한 목표 셀 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/Cell/StoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StoreTableViewCell.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/08.
+//
+
+import UIKit
+
+class StoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
