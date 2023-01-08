d0394cbd175abe6f459c7c338340b044792a07f1
lsh328328 <lsh328328@naver.com>
2023-01-09 00:37:40 +0900

    Refator [#35] LaunchScreen 구현 및 화면전환 구현

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Splash/Controller/SplashViewController.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Splash/Controller/SplashViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import KakaoSDKAuth
+import KakaoSDKUser
+
 class SplashViewController: UIViewController {
     
     private let splashView: SplashView = SplashView()
@@ -19,5 +22,20 @@ class SplashViewController: UIViewController {
         super.loadView()
         
         self.view = splashView
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            if AuthApi.hasToken() {
+                let homeViewController: HomeViewController = HomeViewController()
+                
+                if let window = self.view.window?.windowScene?.keyWindow {
+                    window.rootViewController = homeViewController
+                }
+            } else {
+                let loginViewController: LoginViewController = LoginViewController()
+                
+                if let window = self.view.window?.windowScene?.keyWindow {
+                    window.rootViewController = loginViewController
+                }
+            }
+        }
     }
 }
