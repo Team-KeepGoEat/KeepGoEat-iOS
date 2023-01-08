98a176bfc9e77c15bf06d14f01382662ff8d1605
jeongkite <jeong6kite@gmail.com>
2023-01-08 18:58:02 +0900

    Style [#37] 테이블뷰 셀 내부의 UI 컴포넌트 일부 추가

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/Cell/StoreTableViewCell.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/Cell/StoreTableViewCell.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class StoreTableViewCell: UITableViewCell {
-
+    
+    // MARK: - Variables
+    // MARK: Identifier
+    static let identifier = "StoreTableViewCell"
+    
+    // MARK: Component
+    private let kindTagImage = UIImageView().then {
+        $0.image = Const.Image.moreTag
+    }
+    private let goalContentLabel = UILabel().then {
+        $0.font = .system2Bold
+        $0.text = "오늘의 목표 내용"
+        $0.textColor = .gray800
+    }
+    
+    // MARK: - Function
+    // MARK: LifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
