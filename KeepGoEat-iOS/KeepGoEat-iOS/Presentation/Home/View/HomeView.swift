526c4d44d77047bc810a2783974f8fd093bd1663
jeongkite <jeong6kite@gmail.com>
2023-01-08 16:43:45 +0900

    Delete [#37] 불필요한 공백 삭제

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeView.swift
@@ -23,7 +23,6 @@ class HomeView: UIView {
         
         setLayout()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -39,7 +38,6 @@ class HomeView: UIView {
             self.backgroundColor = .homeBg2
         }
     }
-    
     private func setLayout() {
         self.addSubviews(
             homeCheerView,
