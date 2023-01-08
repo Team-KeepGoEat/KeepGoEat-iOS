ae2349a51dcc31e900b12461bd577e4be1fdf525
jeongkite <jeong6kite@gmail.com>
2023-01-09 03:37:45 +0900

    Style [#37] 보관한 목표 테이블뷰 셀 컴포넌트 추가

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/Cell/StoreTableViewCell.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/Cell/StoreTableViewCell.swift
@@ -14,6 +14,7 @@ class StoreTableViewCell: UITableViewCell {
     static let identifier = "StoreTableViewCell"
     
     // MARK: Component
+    private let shadowBaseView = UIView()
     private let kindTagImage = UIImageView().then {
         $0.image = Const.Image.moreTag
     }
@@ -21,6 +22,26 @@ class StoreTableViewCell: UITableViewCell {
         $0.font = .system2Bold
         $0.text = "오늘의 목표 내용"
         $0.textColor = .gray800
+    }
+    private let totalSubLabel = UILabel().then {
+        $0.font = .system5
+        $0.text = "총 달성일"
+        $0.textColor = .gray500
+    }
+    private let totalCountLabel = UILabel().then {
+        $0.font = .systemDate
+        $0.text = "300"
+        $0.textColor = .green600
+    }
+    private let termSubLabel = UILabel().then {
+        $0.font = .system5
+        $0.text = "진행기간"
+        $0.textColor = .gray500
+    }
+    private let termCountLabel = UILabel().then {
+        $0.font = .system5
+        $0.text = "2022. 09. 01 ~ 2022. 12. 31"
+        $0.textColor = .gray700
     }
     
     // MARK: - Function
