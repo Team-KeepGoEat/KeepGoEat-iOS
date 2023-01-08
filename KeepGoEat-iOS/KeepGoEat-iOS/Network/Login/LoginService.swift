fd1e1cccd34f86e43d48e1edc5aa9ddc8a7d6327
lsh328328 <lsh328328@naver.com>
2023-01-09 02:24:20 +0900

    Feat [#35] 소셜 로그인 로직 구현

--- KeepGoEat-iOS/KeepGoEat-iOS/Network/Login/LoginService.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Network/Login/LoginService.swift
@@ -5,4 +5,42 @@
 //  Created by 양정연 on 2022/12/27.
 //
 
-import Foundation
+import Moya
+
+final class LoginService {
+    static let shared = LoginService()
+    private let loginProvider = MoyaProvider<LoginRouter>(plugins: [NetworkLoggerPlugin()])
+    private init() { }
+}
+
+extension LoginService {
+    func postSocialLogin(param: LoginRequestDto) {
+        loginProvider.request(.socialLogin(param: param)) { response in
+            switch response {
+            case .success(let result):
+                let status = result.statusCode
+                let data = result.data
+                let networkData = NetworkBase.judgeStatus(by: status, data, LoginResponseDto.self)
+                switch networkData {
+                case .success(let data):
+                    guard let data = data as? LoginResponseDto else { return }
+                    addUserTokenOnKeyChain(tokenName: Const.String.userAccessToken, tokenContent: data.accessToken)
+                    addUserTokenOnKeyChain(tokenName: Const.String.userRefreshToken, tokenContent: data.refreshToken)
+                    print(data)
+                case .requestErr(_):
+                    print("request error")
+                case .pathErr:
+                    print("path error")
+                case .serverErr:
+                    print("server error")
+                case .networkFail:
+                    print("network fail error")
+                case .authErr(_):
+                    print("auth error")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
