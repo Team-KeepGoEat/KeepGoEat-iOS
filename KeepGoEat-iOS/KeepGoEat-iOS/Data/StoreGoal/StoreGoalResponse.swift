037331b68f4d714f2b6d837079ef7e8f373a2b89
jeongkite <jeong6kite@gmail.com>
2023-01-09 15:00:07 +0900

    Feat [#37] 보관한 목표 더미데이터 생성

--- KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
@@ -22,3 +22,14 @@ struct StoreGoal: Codable {
     let startedAt, keptAt: String
     let isAchieved: Bool
 }
+
+let getStoreGoalDataList: [GetStoreGoalResponse] = [
+    GetStoreGoalResponse(goals: [], goalCount: 0),
+    GetStoreGoalResponse(goals: [
+        StoreGoal(goalID: 1, goalContent: "양상추", isMore: true, isOngoing: false, writerID: 1, totalCount: 25, startedAt: "2021. 04. 10", keptAt: "2023. 01. 04", isAchieved: false),
+        StoreGoal(goalID: 2, goalContent: "야식", isMore: false, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
+        StoreGoal(goalID: 2, goalContent: "물", isMore: true, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
+        StoreGoal(goalID: 2, goalContent: "아침", isMore: true, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
+        StoreGoal(goalID: 2, goalContent: "저녁 두 숟갈", isMore: false, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false)
+    ], goalCount: 5)
+]
