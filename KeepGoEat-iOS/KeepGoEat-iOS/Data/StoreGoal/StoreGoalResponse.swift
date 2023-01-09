62a84a23430b6c7e3c9a145698f5c3e593a3d2c6
jeongkite <jeong6kite@gmail.com>
2023-01-09 14:59:47 +0900

    Feat [#37] 보관한 목표 모델 생성

--- KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: - GetStoreGoalResponse
+struct GetStoreGoalResponse: Codable {
+    let goals: [StoreGoal]
+    let goalCount: Int
+}
+
+// MARK: Goal
+struct StoreGoal: Codable {
+    let goalID: Int
+    let goalContent: String
+    let isMore, isOngoing: Bool
+    let writerID, totalCount: Int
+    let startedAt, keptAt: String
+    let isAchieved: Bool
+}
