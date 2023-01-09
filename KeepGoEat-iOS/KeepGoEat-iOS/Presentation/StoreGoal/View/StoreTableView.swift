46bbbe286d8c008175c0c7795c1804c88bd4918f
jeongkite <jeong6kite@gmail.com>
2023-01-09 16:51:25 +0900

    Feat [#37] 테이블뷰 기초 코드 작성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreTableView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreTableView.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class StoreTableView: UITableView {
-
     
+    // MARK: - Variables
+    let data = getStoreGoalDataList[1]
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+        setRegister()
+        setUI()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setRegister() {
+        self.register(UITableViewCell.self, forCellReuseIdentifier: StoreTableViewCell.identifier)
+    }
+    private func setUI() {
+        
+    }
+}
 
 }
