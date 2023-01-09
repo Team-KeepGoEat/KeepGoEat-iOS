0976eaca947c66a5b2788067658c590a2e063d6d
jeongkite <jeong6kite@gmail.com>
2023-01-09 16:52:37 +0900

    Chore [#37] 함수 및 변수명 정리

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreTableView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/StoreGoal/View/StoreTableView.swift
@@ -10,23 +10,24 @@ import UIKit
 class StoreTableView: UITableView {
     
     // MARK: - Variables
-    let data = getStoreGoalDataList[1]
     
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
         
         setRegister()
-        setUI()
+        setLayout()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     private func setRegister() {
+        self.delegate = self
         self.register(UITableViewCell.self, forCellReuseIdentifier: StoreTableViewCell.identifier)
     }
-    private func setUI() {
-        
+    private func setLayout() {
+        self.contentInset.left = 16
+        self.contentInset.right = 16
     }
 }
 
