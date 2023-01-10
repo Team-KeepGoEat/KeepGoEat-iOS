8f58f08b6e35d863303f0963e03808a36c75cf94
jeongkite <jeong6kite@gmail.com>
2023-01-10 13:29:33 +0900

    Style [#19] 컴포넌트 생성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeNewGoalTypeView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeNewGoalTypeView.swift
@@ -10,9 +10,16 @@ import UIKit
 class HomeNewGoalTypeView: UIView {
 
     // MARK: - Variables
+    private let eatType: EatType
+    
     // MARK: Component
     private let titleLabel = UILabel().then {
-        $0.text = Const.String.moreEat
+        $0.font = .system3Bold
     }
+    private let subLabel = UILabel().then {
+        $0.font = .system5
+        $0.textColor = .gray600
+    }
+    private let snailImage = UIImageView()
 
 }
