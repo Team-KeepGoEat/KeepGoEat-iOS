90e4d78a55219607f7e238043bf2075c9bfc6239
jeongkite <jeong6kite@gmail.com>
2023-01-10 13:30:13 +0900

    Style [#19] 홈 바텀시트 버튼 UI 스타일 구성

--- KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeNewGoalTypeView.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeNewGoalTypeView.swift
@@ -21,5 +21,40 @@ class HomeNewGoalTypeView: UIView {
         $0.textColor = .gray600
     }
     private let snailImage = UIImageView()
-
+    
+    init(frame: CGRect, eatType: EatType) {
+        self.eatType = eatType
+        super.init(frame: frame)
+        
+        setUI(eatType: eatType)
+        setAddTarget(eatType: eatType)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI(eatType: EatType) {
+        switch eatType {
+        case .more:
+            titleLabel.text = Const.String.moreEat
+            titleLabel.textColor = .orange600
+            subLabel.text = Const.String.homeNewBottomSubMore
+            snailImage.image = Const.Image.snailOrangeSpoon
+        case .less:
+            titleLabel.text = Const.String.lessEat
+            titleLabel.textColor = .green600
+            subLabel.text = Const.String.homeNewBottomSubLess
+            snailImage.image = Const.Image.snailGreenRibon
+        }
+    }
+    private func setAddTarget(eatType: EatType) {
+        switch eatType {
+        case .more:
+            // TODO: addTarget 함수 연결
+            print("더 먹기")
+        case .less:
+            // TODO: addTarget 함수 연결
+            print("덜 먹기")
+        }
+    }
 }
