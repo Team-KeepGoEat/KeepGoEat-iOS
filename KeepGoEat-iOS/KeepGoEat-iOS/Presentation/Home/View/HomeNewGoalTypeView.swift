400de58d1e1fd0e424da5143efbdd10334860507
jeongkite <jeong6kite@gmail.com>
2023-01-10 13:13:30 +0900

    Add [#19] 바텀시트 내부 버튼 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeNewGoalTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeNewGoalTypeView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/10.
+//
+
+import UIKit
+
+class HomeNewGoalTypeView: UIView {
+
+    // MARK: - Variables
+    // MARK: Component
+    private let titleLabel = UILabel().then {
+        $0.text = Const.String.moreEat
+    }
+
+}
