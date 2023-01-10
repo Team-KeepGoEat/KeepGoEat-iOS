54825a811d7ffcd77a221b79ffe68816985be8d5
jeongkite <jeong6kite@gmail.com>
2023-01-11 02:46:22 +0900

    Add [#19] 바텀시트뷰 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/View/HomeBottomSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeBottomSheetView.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/10.
+//
+
+import UIKit
+
+class HomeBottomSheetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
