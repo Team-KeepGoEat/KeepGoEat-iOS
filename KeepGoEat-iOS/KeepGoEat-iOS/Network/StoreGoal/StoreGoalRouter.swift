46283f17b1f9a110731385b38401e42db4d7c123
jeongkite <jeong6kite@gmail.com>
2023-01-11 22:10:19 +0900

    Feat [#54] StoreGoalRouter

--- KeepGoEat-iOS/KeepGoEat-iOS/Network/StoreGoal/StoreGoalRouter.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Network/StoreGoal/StoreGoalRouter.swift
@@ -5,4 +5,37 @@
 //  Created by 양정연 on 2022/12/30.
 //
 
-import Foundation
+import Moya
+
+enum StoreGoalRouter {
+    case getMypage(sortType: String)
+}
+
+extension StoreGoalRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .getMypage(sortType: let sortType):
+            return URLConstant.storeGoal + sortType
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getMypage:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getMypage:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.accessTokenHeader
+    }
+    
+    
+}
