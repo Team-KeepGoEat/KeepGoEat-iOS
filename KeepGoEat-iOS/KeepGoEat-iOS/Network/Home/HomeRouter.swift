f17accf2dc2e4dbbb8f0ce65c2d0662be0fbd925
jeongkite <jeong6kite@gmail.com>
2023-01-12 02:16:50 +0900

    Feat [#67] getHome Router

--- KeepGoEat-iOS/KeepGoEat-iOS/Network/Home/HomeRouter.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Network/Home/HomeRouter.swift
@@ -5,4 +5,35 @@
 //  Created by 양정연 on 2022/12/27.
 //
 
-import Foundation
+import Moya
+
+enum HomeRouter {
+    case getHome
+}
+
+extension HomeRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .getHome:
+            return URLConstant.getHome
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getHome:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getHome:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return NetworkConstant.accessTokenHeader
+    }
+}
