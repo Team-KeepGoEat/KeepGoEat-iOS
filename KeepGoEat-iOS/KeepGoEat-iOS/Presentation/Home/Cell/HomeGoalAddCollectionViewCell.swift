45341c5ed1fd3364cfbd934bcbf21ba40a9858d1
jeongkite <jeong6kite@gmail.com>
2023-01-11 12:35:54 +0900

    Add [#19] 목표 추가 컬렉션뷰셀 파일 생성

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Presentation/Home/Cell/HomeGoalAddCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeGoalAddCollectionViewCell.swift
+//  KeepGoEat-iOS
+//
+//  Created by 양정연 on 2023/01/11.
+//
+
+import UIKit
+
+class HomeGoalAddCollectionViewCell: UICollectionViewCell {
+    
+}
