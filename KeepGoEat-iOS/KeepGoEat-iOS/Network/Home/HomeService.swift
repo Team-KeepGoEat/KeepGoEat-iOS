7815c9cc99fc53a8924ba92b50e6ee2d46ae2e71
jeongkite <jeong6kite@gmail.com>
2023-01-12 02:17:09 +0900

    Feat [#67] getHome Service

--- KeepGoEat-iOS/KeepGoEat-iOS/Network/Home/HomeService.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Network/Home/HomeService.swift
@@ -5,4 +5,43 @@
 //  Created by 양정연 on 2022/12/27.
 //
 
-import Foundation
+import Moya
+
+final class HomeService {
+    static let shared = HomeService()
+    private let homeProvider = MoyaProvider<HomeRouter>(plugins: [MoyaLoggingPlugin()])
+    private init() { }
+}
+
+extension HomeService {
+    func getHome(completion: @escaping (GetHomeResponse?) -> Void) {
+        homeProvider.request(.getHome) { response in
+            switch response {
+            case .success(let result):
+                let status = result.statusCode
+                let data = result.data
+                let networkData = NetworkBase.judgeStatus(by: status, data, GetHomeResponse.self)
+                switch networkData {
+                case .success(let data):
+                    guard let data = data as? GetHomeResponse else { return }
+                    completion(data)
+                case .requestErr(let data):
+                    guard let data = data as? String else { return }
+                    print(data)
+                case .authErr(let data):
+                    guard let data = data as? String else { return }
+                    print(data)
+                case .serverErr(let data):
+                    guard let data = data as? String else { return }
+                    print(data)
+                case .pathErr:
+                    print("path error")
+                case .networkFail:
+                    print("network fail error")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
