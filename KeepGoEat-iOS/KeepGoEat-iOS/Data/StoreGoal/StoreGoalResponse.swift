4321790d79db3fe297707d8a03008114f95e6c5f
jeongkite <jeong6kite@gmail.com>
2023-01-11 21:08:33 +0900

    Chore [#54] getStoreGoalDataList

--- KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/StoreGoal/StoreGoalResponse.swift
@@ -22,19 +22,22 @@ struct GetStoreGoalResponse: Codable {
 struct StoreGoal: Codable, Hashable {
     let goalID: Int
     let goalContent: String
-    let isMore, isOngoing: Bool
-    let writerID, totalCount: Int
-    let startedAt, keptAt: String
+    let isMore: Bool
+    let isOngoing: Bool
+    let totalCount: Int
+    let startedAt: String
+    let keptAt: String
     let isAchieved: Bool
+    let writerID: Int
 }
 
 let getStoreGoalDataList: [GetStoreGoalResponse] = [
     GetStoreGoalResponse(goals: [], goalCount: 0),
     GetStoreGoalResponse(goals: [
-        StoreGoal(goalID: 1, goalContent: "양상추", isMore: true, isOngoing: false, writerID: 1, totalCount: 25, startedAt: "2021. 04. 10", keptAt: "2023. 01. 04", isAchieved: false),
-        StoreGoal(goalID: 2, goalContent: "야식", isMore: false, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
-        StoreGoal(goalID: 3, goalContent: "물", isMore: true, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
-        StoreGoal(goalID: 4, goalContent: "아침", isMore: true, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false),
-        StoreGoal(goalID: 5, goalContent: "저녁 두 숟갈", isMore: false, isOngoing: false, writerID: 1, totalCount: 16, startedAt: "2022. 12. 01", keptAt: "2022. 12. 31", isAchieved: false)
+        StoreGoal(goalID: 1, goalContent: "양상추", isMore: true, isOngoing: false, totalCount: 275, startedAt: "2021. 04. 10", keptAt: "2023. 01. 04", isAchieved: false, writerID: 1),
+        StoreGoal(goalID: 2, goalContent: "야식", isMore: false, isOngoing: false, totalCount: 38, startedAt: "2022. 11. 09", keptAt: "2022. 12. 31", isAchieved: false, writerID: 1),
+        StoreGoal(goalID: 3, goalContent: "물", isMore: true, isOngoing: false, totalCount: 23, startedAt: "2022. 10. 14", keptAt: "2022. 12. 20", isAchieved: false, writerID: 1),
+        StoreGoal(goalID: 4, goalContent: "아침", isMore: true, isOngoing: false, totalCount: 4, startedAt: "2022. 06. 04", keptAt: "2022. 09. 06", isAchieved: false, writerID: 1),
+        StoreGoal(goalID: 5, goalContent: "저녁 두 숟갈", isMore: false, isOngoing: false, totalCount: 16, startedAt: "2022. 02. 06", keptAt: "2022. 04. 10", isAchieved: false, writerID: 1)
     ], goalCount: 5)
 ]
