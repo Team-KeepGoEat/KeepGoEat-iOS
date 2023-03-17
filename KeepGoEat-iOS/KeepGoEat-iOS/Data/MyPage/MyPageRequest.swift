8e2dc07bc2a8ab158fc5dc660374bdfbfb2ae664
정채은 <cce523@hanmail.net>
2023-03-18 00:36:16 +0900

    Add [#111] 파일 생성 및 URLConstant 추가 및 수정

---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/MyPage/MyPageRequest.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageRequest.swift
+//  KeepGoEat-iOS
+//
+//  Created by 정채은 on 2023/03/17.
+//
+
+import Foundation
---
+++ KeepGoEat-iOS/KeepGoEat-iOS/Data/MyPage/MyPageResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageResponse.swift
+//  KeepGoEat-iOS
+//
+//  Created by 정채은 on 2023/03/17.
+//
+
+import Foundation
